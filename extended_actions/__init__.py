53c3e68c9f466e7f398663f3853b37accfbe6d58
Dmitry Kolyagin <dmitry.kolyagin@gmail.com>
2020-08-02 16:46:47 +0300

    Fix action params form template

--- extended_actions/__init__.py
+++ extended_actions/__init__.py
@@ -13,7 +13,10 @@ default_app_config = 'extended_actions.apps.ExtendedActionsConfig'
 
 
 class FormMixin(forms.Form):
-    _selected_action = forms.CharField(widget=forms.MultipleHiddenInput)
+    def __init__(self, *args, **kwargs):
+        self.queryset = kwargs.pop('queryset')
+        super(FormMixin, self).__init__(*args, **kwargs)
+        self.fields[admin.ACTION_CHECKBOX_NAME] = forms.CharField(widget=forms.MultipleHiddenInput)
 
 
 def extended_action(form_class):  # type: (Type[forms.Form]) -> Callable
@@ -21,20 +24,25 @@ def extended_action(form_class):  # type: (Type[forms.Form]) -> Callable
         pass
 
     def _decorator(action_method):
-        def _wrapper(self, request, queryset):
+        def _wrapper(model_admin, request, queryset):
             # type: (admin.ModelAdmin, HttpRequest, models.QuerySet) -> HttpResponse
             form = None
-            if 'perform' in request.POST:
-                form = _Form(request.POST)
+            action_submit_parameter = 'perform'
+            if action_submit_parameter in request.POST:
+                form = _Form(request.POST, queryset=queryset)
                 if form.is_valid():
-                    action_method(self, request, queryset, form)
+                    action_method(model_admin, request, queryset, form)
                 return HttpResponseRedirect(request.get_full_path())
             if not form:
-                form = _Form(initial={'_selected_action': request.POST.getlist(admin.ACTION_CHECKBOX_NAME)})
+                form = _Form(
+                    initial={'_selected_action': request.POST.getlist(admin.ACTION_CHECKBOX_NAME)},
+                    queryset = queryset
+                )
             return render(request, 'extended_actions/form.html', context={
                 'items': queryset,
                 'form': form,
                 'action': action_method.__name__,
+                'action_submit_parameter':  action_submit_parameter,
             })
         return _wrapper
 
